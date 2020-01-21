0c2fba4f08a9f638e880a7ac4dbd08604da7f172
Pooja Wasnik <wasnikpooja24@gmail.com>
2020-01-21 17:58:39 +0530

    js basics

---
+++ basics/var_fn.js
@@ -0,0 +1,9 @@
+var num = 10
+var num1 = 20
+console.log(num+num1)
+
+function calc(n1,n2)
+{
+ console.log(n1+n2)
+}
+calc(100,200)
---
+++ es6/new_js.js
@@ -0,0 +1,27 @@
+var n= 10
+if(true)
+{
+    var n=20;
+}
+console.log(n)
+
+let n1= 10
+if(true)
+{
+   let n1=20;
+}
+console.log(n1)
+
+let data=`truyi uwyeiuwei 
+          gfdjhsd uuyuyjhgsdjg jhdfj`
+
+          function dataval(dt)
+          {
+           return dt.toUpperCase() + Date.now()
+          }
+let datavalue = dataval(data)
+console.log(datavalue)
+
+let lambda = dt => dt.toUpperCase() + Date.now()
+console.log(lambda(`learning angular is easy`)
+//console.log(lambda)
